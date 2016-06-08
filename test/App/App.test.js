755600b2f3e887da1e809385ccad0a7528d8a965
Adheesh Bhatia <adheeshbhatia@gmail.com>
2016-06-08 15:12:35 +0530

    Added basic tests for App component

---
+++ test/App/App.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import log from 'loglevel';
+
+import App from '../../src/app.component.js';
+import HeaderBar from 'd2-ui/lib/header-bar/HeaderBar.component';
+import Sidebar from 'd2-ui/lib/sidebar/Sidebar.component';
+
+describe('App', () => {
+    let appComponent;
+
+    beforeEach(() => {
+        appComponent = shallow(<App route={{path:'user'}} d2={{}}/>);
+    });
+
+    it('should render the HeaderBar component from d2-ui', () => {
+        expect(appComponent.find(HeaderBar)).to.have.length(1);
+    });
+
+    it('should render the Sidebar component from d2-ui', () => {
+        expect(appComponent.find(Sidebar)).to.have.length(1);
+    });
+
+    it('should pass three sections to the Sidebar component', () => {
+        const sidebarComponent = appComponent.find(Sidebar);
+
+        expect(sidebarComponent.props().sections).to.have.length(3);
+    });
+
+    it('should pass the key and label properties for the sections', () => {
+        const sidebarComponent = appComponent.find(Sidebar);
+        const sidebarSections = sidebarComponent.props().sections;
+
+        expect(sidebarSections).to.deep.equal([
+            { key: 'profile', label: 'Profile Settings' },
+            { key: 'account', label: 'Account Settings' },
+            { key: 'user', label: 'User Settings' },
+        ]);
+    });
+});
