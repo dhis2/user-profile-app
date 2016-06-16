e404720cb097a1908241caa80126d993e6769e5c
Adheesh Bhatia <adheeshbhatia@gmail.com>
2016-06-16 14:48:44 +0530

    Basic view profile ui

--- src/profile/profile.component.js
+++ src/profile/profile.component.js
@@ -13,6 +13,24 @@ import AppTheme from '../theme';
 
 import profileActions from './profileActions'
 
+const styles = {
+    header: {
+        fontSize: 24,
+        fontWeight: 100,
+        color: AppTheme.rawTheme.palette.textColor,
+        padding: '6px 16px',
+    },
+    card: {
+        marginTop: 8,
+        margin: '1rem',
+        maxWidth: '600px',
+    },
+    fieldValue: {
+        fontSize: 20,
+        fontWeight: 100,
+    },
+};
+
 class Profile extends React.Component {
     constructor(props, context){
         super(props);
@@ -20,6 +38,8 @@ class Profile extends React.Component {
         this.userId = this.props.params.userID;
         this.d2 = this.props.route.d2;
         if(!this.userId){ hashHistory.push('profile'); };
+        this.state = Object.assign({});
+        this.renderFields = this.renderFields.bind(this);
     }
 
     getChildContext() {
@@ -35,7 +55,7 @@ class Profile extends React.Component {
         this.subscriptions.push(profileActions.getProfile.subscribe((arg) => {
             this.d2.Api.getApi().get(`users/${arg.data}`)
             .then( res => { 
-                console.log(res)
+                this.setState(res);
             })
             .catch ( err => {
                 hashHistory.push('profile');
@@ -53,10 +73,57 @@ class Profile extends React.Component {
         });
     }
 
+    renderFields() {
+        const fields = [
+            'email',
+            'phoneNumber',
+            'introduction',
+            'jobTitle',
+            'gender',
+            'birthday',
+            'nationality',
+            'employer',
+            'education',
+            'interests',
+            'languages',
+        ]
+
+        const labels = {
+            email: 'email',
+            phoneNumber: 'phone_number',
+            introduction: 'introduction',
+            jobTitle: 'job_title',
+            gender: 'gender',
+            birthday: 'birthday',
+            nationality: 'nationality',
+            employer: 'employer',
+            education: 'education',
+            interests: 'interests',
+            languages: 'languages',
+        }
+
+        const profileFields = fields.map((key) => {
+            const label = labels[key];
+            return (<CardText key={key}>
+                        <div style={styles.fieldKey}>{this.d2.i18n.getTranslation(label)}</div>
+                        {this.state[key] ? <div style={styles.fieldValue}>{this.state[key]}</div> : <div style={styles.fieldValue} >Not Avaialble</div>}
+                    </CardText>);
+        });
+
+        return profileFields;
+    }
+
     render() {
+
+        const renderFields = this.renderFields();
+
         return (
             <div className="app-wrapper">
                 <HeaderBar />
+                <div style={styles.header}>{this.state.displayName}</div>
+                <Card style={styles.card} key={this.props.category}>
+                    { renderFields }
+                </Card>
             </div>
         );
     }
