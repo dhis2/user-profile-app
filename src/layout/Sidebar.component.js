ee0e6dab02204173f574e7e4759b6ca7f791379b
Nicolay Ramm <nicolay@dhis2.org>
2016-09-01 11:00:50 +0200

    Put the user settings section first in the sidebar

--- src/layout/Sidebar.component.js
+++ src/layout/Sidebar.component.js
@@ -5,8 +5,8 @@ import appActions from '../app.actions';
 
 function SidebarWrapper(props, context) {
     const sideBarSections = [
-        { key: 'profile', label: context.d2.i18n.getTranslation('user_profile'), icon: 'face' },
         { key: 'settings', label: context.d2.i18n.getTranslation('user_settings'), icon: 'build' },
+        { key: 'profile', label: context.d2.i18n.getTranslation('user_profile'), icon: 'face' },
         { key: 'account', label: context.d2.i18n.getTranslation('account_settings'), icon: 'account_circle' },
     ];
 
