f3f985b725b222ae86ed8aa2193a71dcb1bc9857
Hendrik de Graaf <hendrik@dhis2.org>
2018-10-15 17:37:17 +0200

    Tweak about page: - Fields match old about page at `/dhis-web-commons-about/about.action` - Field visibility is controlled by the server, if no value is found, the field is not rendered - Added a formatter for date-time fields

--- src/aboutPage/AboutPage.component.js
+++ src/aboutPage/AboutPage.component.js
@@ -3,30 +3,77 @@ import PropTypes from 'prop-types';
 
 import AboutSection from './AboutSection.component';
 
+const parseDateFromUTCString = (utcString, d2) => {
+    const locale = d2.currentUser.userSettings.settings.keyUiLocale;
+    const date = new Date(utcString);
+    const options = {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    };
+    return new Intl.DateTimeFormat(locale, options).format(date);
+};
+
 const attributes = {
     systemInfo: [
+        {
+            label: 'web_api',
+            getDisplayValue: (_value, d2, translate) => (
+                <a target="_blank" href={`${d2.system.systemInfo.contextPath}/api`}>
+                    {translate('browse_it_here')}
+                </a>
+            ),
+        },
+        {
+            label: 'current_user',
+            getDisplayValue: (_value, d2) => d2.currentUser.username,
+        },
         'version',
         'revision',
-        'buildTime',
-        'osName',
-        'osVersion',
-        'osArchitecture',
-        'serverDate',
-        'cpuCores',
-        'memoryInfo',
-        'javaVersion',
-        'lastAnalyticsTableRuntime',
-        'lastAnalyticsTableSuccess',
-        'fileStoreProvider',
+        {
+            label: 'buildTime',
+            getDisplayValue: (value, d2) => parseDateFromUTCString(value, d2),
+        },
         'jasperReportsVersion',
-        'calendar',
+        'userAgent',
+        {
+            label: 'serverDate',
+            getDisplayValue: (value, d2) => parseDateFromUTCString(value, d2),
+        },
+        {
+            label: 'lastAnalyticsTableSuccess',
+            getDisplayValue: (value, d2) => parseDateFromUTCString(value, d2),
+        },
+        'intervalSinceLastAnalyticsTableSuccess',
+        'lastAnalyticsTableRuntime',
+        'environmentVariable',
+        'systemId',
+        {
+            label: 'lastSystemMonitoringSuccess',
+            getDisplayValue: (value, d2) => parseDateFromUTCString(value, d2),
+        },
+        'externalDirectory',
+        'fileStoreProvider',
+        'nodeId',
         'cacheProvider',
-         'userAgent',
+        'readReplicaCount',
+        'javaOpts',
+        'javaVersion',
+        'javaVendor',
+        'osName',
+        'osArchitecture',
+        'osVersion',
+        'memoryInfo',
+        'cpuCores',
+        'calendar',
     ],
     databaseInfo: [
         'name',
         'user',
-        'type',
+        'spatialSupport',
     ],
 };
 
@@ -34,13 +81,33 @@ class AboutPage extends Component {
     translate = s => this.context.d2.i18n.getTranslation(s);
 
     getAttributes = (selected, source) => selected
-        .map(attribute => ({
-            label: this.translate(attribute),
-            value: source[attribute],
-        }))
-        .filter(attribute => 
-            attribute.value !== undefined
-        );
+        .reduce((acc, attribute) => {
+            const value = typeof attribute === 'object' ? 
+                source[attribute.label] : 
+                source[attribute];
+            
+            if (!value) {
+                return acc;
+            }
+
+            if (typeof attribute === 'object') {
+                acc.push({
+                    label: this.translate(attribute.label),
+                    value: attribute.getDisplayValue(
+                        value,
+                        this.context.d2,
+                        this.translate
+                    ),
+                })
+            } else {
+                acc.push({
+                    label: this.translate(attribute),
+                    value,
+                })
+            }
+
+            return acc;
+        }, []);
 
     render = () => {
         const { d2 } = this.context;
