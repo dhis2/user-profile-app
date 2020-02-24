018e0594c36c9b29da34b0cac56036bca09f36d4
Hendrik de Graaf <hendrik@dhis2.org>
2020-02-24 11:32:30 +0100

    fix(about-page): render undefined date strings as "N/A" (v32)

--- src/aboutPage/AboutPage.component.js
+++ src/aboutPage/AboutPage.component.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 
 import AboutSection from './AboutSection.component';
 
+
+
 const parseDateFromUTCString = (utcString, d2) => {
+    if (!utcString) {
+        return d2.i18n.getTranslation('not_applicable')
+    }
+
     const locale = d2.currentUser.userSettings.settings.keyUiLocale;
     const date = new Date(utcString);
     const options = {
