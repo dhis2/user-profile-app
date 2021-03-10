ad0796051fe193cc4436f048f196bd71c6e8c889
Kai Vandivier <kpvandivier@gmail.com>
2021-03-10 18:47:08 +0100

    refactor: fix up src/profile

--- src/profile/isValidWhatsApp.js
+++ src/profile/isValidWhatsApp.js
@@ -1,7 +1,7 @@
-const PHONE_PATTERN = /^\+(?:[0-9].?){4,14}[0-9]$/;
+const PHONE_PATTERN = /^\+(?:[0-9].?){4,14}[0-9]$/
 
 export default function isValidWhatsApp(value) {
     // It's an optional field so empty is also valid
     return !value || PHONE_PATTERN.test(value)
 }
-isValidWhatsApp.message = 'invalid_whats_app';
+isValidWhatsApp.message = 'invalid_whats_app'
--- src/profile/profile.actions.js
+++ src/profile/profile.actions.js
@@ -1,33 +1,34 @@
-import { getInstance as getD2 } from 'd2';
-import Action from 'd2-ui/lib/action/Action';
-import { wordToValidatorMap } from 'd2-ui/lib/forms/Validators';
-import log from 'loglevel';
-import userSettingsActions from '../app.actions';
-import userSettingsKeyMapping from '../userSettingsMapping';
-import isValidWhatsApp from './isValidWhatsApp';
-import userProfileStore from './profile.store';
+import { getInstance as getD2 } from 'd2'
+import Action from 'd2-ui/lib/action/Action'
+import { wordToValidatorMap } from 'd2-ui/lib/forms/Validators'
+import log from 'loglevel'
+import userSettingsActions from '../app.actions'
+import i18n from '../locales'
+import userSettingsKeyMapping from '../userSettingsMapping'
+import isValidWhatsApp from './isValidWhatsApp'
+import userProfileStore from './profile.store'
 
 // Add whatsApp validation to the validator set
-wordToValidatorMap.set('whats_app', isValidWhatsApp);
+wordToValidatorMap.set('whats_app', isValidWhatsApp)
 
-const userProfileActions = Action.createActionsFromNames(['save']);
+const userProfileActions = Action.createActionsFromNames(['save'])
 
 userProfileActions.save.subscribe(({ data, complete, error }) => {
-    const [key, value] = data;
-    let payload = { [key]: String(value).trim() || ' ' }; // TODO: Ugly hack to allow saving empty fields
+    const [key, value] = data
+    let payload = { [key]: String(value).trim() || ' ' } // TODO: Ugly hack to allow saving empty fields
 
     if (key === 'birthday') {
-        const date = new Date(value);
-        payload = { [key]: date.toISOString() };
+        const date = new Date(value)
+        payload = { [key]: date.toISOString() }
     }
 
-    getD2().then((d2) => {
-        userProfileStore.state[key] = value;
-        userProfileStore.setState(userProfileStore.state);
+    getD2().then(d2 => {
+        userProfileStore.state[key] = value
+        userProfileStore.setState(userProfileStore.state)
 
         // Run field validators
         if (Array.isArray(userSettingsKeyMapping[key].validators)) {
-            const validators = userSettingsKeyMapping[key].validators;
+            const validators = userSettingsKeyMapping[key].validators
             if (
                 !validators.reduce(
                     (prev, curr) => prev && wordToValidatorMap.get(curr)(value),
@@ -36,8 +37,8 @@ userProfileActions.save.subscribe(({ data, complete, error }) => {
             ) {
                 log.warn(
                     `One or more validators did not pass for field "${key}" and value "${value}"`
-                );
-                return;
+                )
+                return
             }
         }
 
@@ -49,47 +50,45 @@ userProfileActions.save.subscribe(({ data, complete, error }) => {
             d2,
         })
             .then(() => {
-                log.debug('User Profile updated successfully.');
+                log.debug('User Profile updated successfully.')
                 userSettingsActions.showSnackbarMessage({
-                    message: d2.i18n.getTranslation(
-                        'update_user_profile_success'
-                    ),
+                    message: i18n.t('User profile updated'),
                     status: 'success',
-                });
-                complete();
+                })
+                complete()
             })
-            .catch((err) => {
+            .catch(err => {
                 userSettingsActions.showSnackbarMessage({
-                    message: d2.i18n.getTranslation('update_user_profile_fail'),
+                    message: i18n.t('Failed to update user profile'),
                     status: 'error',
-                });
-                log.error('Failed to update user profile:', err);
-                error();
-            });
-    });
-});
+                })
+                log.error('Failed to update user profile:', err)
+                error()
+            })
+    })
+})
 
 function getProfileUpdatePromise({ key, value, payload, userId, d2 }) {
-    const api = d2.Api.getApi();
+    const api = d2.Api.getApi()
     if (key === 'avatar') {
         if (value) {
             // Set avatar
             // Post avatar directly to the `/user/<ID>` endpoint, because d2 update calls to `/me` cause issues
-            return api.patch(`/users/${userId}`, { avatar: value.id });
+            return api.patch(`/users/${userId}`, { avatar: value.id })
         } else {
             // Clear avatar
-            return removeAvatar(userId, api);
+            return removeAvatar(userId, api)
         }
     }
 
-    return api.update('me', payload);
+    return api.update('me', payload)
 }
 
 async function removeAvatar(userId, api) {
-    const url = `/users/${userId}`;
-    const user = await api.get(url, { fields: ':owner' });
-    delete user.avatar;
-    return api.update(url, user);
+    const url = `/users/${userId}`
+    const user = await api.get(url, { fields: ':owner' })
+    delete user.avatar
+    return api.update(url, user)
 }
 
-export default userProfileActions;
+export default userProfileActions
