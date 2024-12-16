d4c0c36b3f29e5109575c9fe50254c117d7c1d29
Chisomchima <chimachisom360@gmail.com>
2024-12-16 06:56:51 +0100

    feat: add the verify email warning

--- src/layout/FormFields.component.js
+++ src/layout/FormFields.component.js
@@ -17,6 +17,7 @@ import userSettingsKeyMapping from '../userSettingsMapping.js'
 import AvatarEditor from './AvatarEditor.component.js'
 import AppTheme from './theme.js'
 import { VerifyEmail } from './VerifyEmail.component.js'
+import  VerifyEmailWarning  from './VerifyEmailWarning.js'
 
 const styles = {
     header: {
@@ -387,6 +388,7 @@ class FormFields extends Component {
             <div className="content-area">
                 <div style={styles.header}>{this.props.pageLabel}</div>
                 <form autoComplete="off">
+                    <VerifyEmailWarning />
                     {this.renderFields(this.props.fieldKeys)}
                 </form>
             </div>
---
+++ src/layout/VerifyEmailWarning.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useDataQuery, useConfig } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import { NoticeBox } from '@dhis2/ui'
+import { useAlert } from '@dhis2/app-runtime'
+
+const getUserProfileQuery = {
+    me: {
+        resource: 'me',
+    },
+}
+const getSystemSettingsQuery = {
+    systemSettings: {
+        resource: 'systemSettings',
+    },
+}
+
+const VerifyEmailWarning = () => {
+    const { data: userData, error: userError } =
+        useDataQuery(getUserProfileQuery)
+    const { data: systemData, error: systemError } = useDataQuery(
+        getSystemSettingsQuery
+    )
+
+    const errorAlert = useAlert(({ message }) => message, { critical: true })
+
+    const enforceVerifiedEmail =
+        systemData?.systemSettings?.enforceVerifiedEmail
+    const emailVerified = userData?.me?.emailVerified
+
+    if (userError || systemError) {
+        errorAlert.show({
+            message: i18n.t('Error fetching user or system data.'),
+        })
+        return null
+    }
+
+    if (enforceVerifiedEmail && !emailVerified) {
+        return (
+            <NoticeBox warning>
+                {i18n.t(
+                    'Your email is not verified. Please verify your email to continue using the system.'
+                )}
+            </NoticeBox>
+        )
+    }
+
+    return null
+}
+
+export default VerifyEmailWarning
